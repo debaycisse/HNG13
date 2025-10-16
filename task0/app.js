326565c482196a9975fbd2dc05cb81afff66c2dd
Azeez Adebayo <debaycisse@gmail.com>
2025-10-16 16:47:19 +0100

    chore (app): add app definition

---
+++ task0/app.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+// app.use(middleware.requestLogger)  // middleware request logger
+// app.use('/me', profileRouter)  // profile router
+// app.use(middleware.unknownEndpoint)
+// app.use(middleware.errorHandler)  // error handler
+
+module.exports = app
