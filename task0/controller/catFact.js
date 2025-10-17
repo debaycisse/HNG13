cff99ad8557143fe73f9dc3ce0e221fca6a097fc
Azeez Adebayo <debaycisse@gmail.com>
2025-10-17 07:11:31 +0100

    chore (api): implement invocation of cat fact api

--- task0/controller/catFact.js
+++ task0/controller/catFact.js
@@ -1,7 +1,8 @@
 const axios = require('axios')
 
 const fact = async () => {
-
+  const catFactObject = await axios.get(process.env.FACT_URL)
+  return catFactObject.data.fact
 }
 
 module.exports = fact
