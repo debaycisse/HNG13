1d4d155489cf1d0348653e6fc48a4b346a17e768
Azeez Adebayo <debaycisse@gmail.com>
2025-10-17 07:14:21 +0100

    chore (app): link endpoint to its url

--- task0/app.js
+++ task0/app.js
@@ -1,12 +1,14 @@
 require('dotenv').config()
 const express = require('express')
+const profileRouter = require('./controller/profile')
 
 const app = express()
 
 app.use(express.json())
 // app.use(middleware.requestLogger)  // middleware request logger
-// app.use('/me', profileRouter)  // profile router
+app.use('/me', profileRouter)  // profile router
 // app.use(middleware.unknownEndpoint)
 // app.use(middleware.errorHandler)  // error handler
+
 
 module.exports = app
