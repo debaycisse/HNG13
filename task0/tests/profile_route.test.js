d20a1d75d1375d98759788a0fb4632e3350ca110
Azeez Adebayo <debaycisse@gmail.com>
2025-10-17 15:29:40 +0100

    test: implement tests for fields requirement

--- task0/tests/profile_route.test.js
+++ task0/tests/profile_route.test.js
@@ -7,14 +7,15 @@ const app = require('../app')
 const api = supertest(app)
 
 // Required endpoint
-describe('/me', async () => {
+describe('/me', () => {
   let response = null
 
   beforeEach(async () => {
     response = await api.get('/me')
+    response = response.body
   })
 
-  describe('endpoint is required', async () => {
+  describe('endpoint is required', () => {
     // to be reachable at /me route
     test('to be reachable at /me', async () => {
       await api
@@ -34,39 +35,65 @@ describe('/me', async () => {
       // const response = await api
       //   .get('/me')
 
-      const responseData = response.body
-      assert(Object.keys(responseData).includes('fact'))
+      assert(Object.keys(response).includes('fact'))
 
-      const catFact = responseData.fact
+      const catFact = response.fact
       assert(catFact !== undefined)
 
       assert(catFact.length > 3)
     })
   })
 
-  describe('field specification', async () => {
+  describe('field specification', () => {
     // status — Must always be the string "success"
-    test('status field must always have the string "success" value',
+    test(
+      'status field must always have the string "success" value',
       async () => {
         // const response = await api.get('/me')
-        assert.strictEqual(
-          response.body.status, "success"
-        )
-    })
+        assert.strictEqual(response.status, "success")
+      }
+    )
 
     // user.email — Your personal email address
     test('user.email field must have an email as value',
       async () => {
-        const emailRegex = /^[1-9]|[a-z]|[A-Z]+/
+        const emailRegex =/(((\w+@)|((\w+-)*(\w+)@)|((\w+\.)*(\w+)@))(\w+|((\w+)-)*)\.[a-zA-Z]+)/
+        assert(emailRegex.test(response.user.email))
+        assert(response.user.email.length >= 5)
     })
 
     // user.name — Your full name
+    test('user.name contains a valid full name', async () => {
+      const nameRegex = /(\w+)|((\w+)\s(\w+))/gi
+      assert(nameRegex.test(response.user.name))
+      assert(response.user.name.length >= 2)
+    })
     
     // user.stack — Your backend technology stack (e.g., "Node.js/Express", "Python/Django", "Go/Gin")
+    test('user.stack contains a backend stack', async () => {
+      const stackRegex = /(\w+|\w+\.\w+)\/\w+/
+      assert(stackRegex.test(response.user.stack))
+      assert(response.user.stack.length > 4)
+    })
     
     // timestamp — Current UTC time in ISO 8601 format (e.g., "2025-10-15T12:34:56.789Z")
-    
+    test('timestamp is dynamic', async () => {
+      const firstTimestamp = response.timestamp
+      const secondResponse = await api.get('/me')
+      const secondTimestamp = secondResponse.body.timestamp
+      assert(firstTimestamp < secondTimestamp)
+    })
+
     // fact — A random cat fact fetched from the Cat Facts API
+    test(
+      'cat\'s fact, fetched from the Cat Facts API is random',
+      async () => {
+        const firstFact = response.fact
+        const secondResponse = await api.get('/me')
+        const secondFact = secondResponse.body.fact
+        assert(firstFact !== secondFact)
+      }
+    )
   })
 
   // describe('timestamp field or property', async () => {
